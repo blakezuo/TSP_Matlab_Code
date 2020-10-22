bd12567072994e0e0838bdfb0409556ffb67d76e
blake_zuo <zuoxc@ruc.edu.cn>
2020-10-22 16:11:09 +0800

    debug

--- main_tsp.m
+++ main_tsp.m
@@ -12,25 +12,28 @@ pop.pm = .3;
 % ±àÂëÎ»Êý
 
 [index,x,y]=textread('mona-lisa100K.tsp.txt','%d %d %d');
+%x=x(1:50);
+%y=y(1:50);
 pop.citynum = size(x,1);
 pop.cityX = x;
 pop.cityY = y;
 fig = figure(1);
 set(fig,'position',[100 100 1000 600]);
 
-plot(x,y,'k.','MarkerSize',1,'LineWidth',2);
+plot(x,y,'k.','MarkerSize',5,'LineWidth',2);
 hold on
 pop=initialize_tsp(pop);
-plot(pop.cityX(pop.bestChro),pop.cityY(pop.bestDisplay), 'k','LineWidth',0.1);
+plot(pop.cityX([pop.bestChro,pop.bestChro(1)]),pop.cityY([pop.bestChro,pop.bestChro(1)]), 'k','LineWidth',0.1);
 minlen = pop.bestlen;
 for i = 1:pop.maxGen
     pop = newPop_tsp(pop);
     figure(1)
-    minlen = min(minlen, pop.bestlen);
-    title("Gen: " + num2str(i) + " CurrentBest " + num2str(pop.bestlen) + "   Best: " + num2str(pop.bestlen));
-    plot(x,y,'k.','MarkerSize',1,'LineWidth',2);
+    plot(x,y,'k.','MarkerSize',2,'LineWidth',2);
     hold on
-    plot(pop.cityX(pop.bestChro),pop.cityY(pop.bestDisplay), 'k','LineWidth',0.1);
+    minlen = min(minlen, pop.bestlen);
+    title('Gen: ' + num2str(i) + ' CurrentBest ' + num2str(pop.bestlen) + '   Best: ' + num2str(minlen));
+    plot(pop.cityX([pop.bestChro,pop.bestChro(1)]),pop.cityY([pop.bestChro,pop.bestChro(1)]), 'k','LineWidth',0.1);
+    hold off
     pause(0.05)
 end
 
